37cd0da174bfce6c50d6ce3ba49ab0e9a593fdc3
mohammd abedini <98052239+Mabedini63@users.noreply.github.com>
2024-02-27 15:12:21 +0330

    Update ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Get lsit fo FTP sever in Shell Script user Here documents
+#after RUN this Script mylist create in curent Directory in you can see list file of in you server
 ftp -n 127.0.0.1 <<END_SCRIPT 
 quote USER abedini
 quote PASS 123
